3bbc0c26b4072e376ca98c5cdd0cb27c1838fb0c
MigVarona <migvaronag@gmail.com>
2023-12-28 19:48:55 +0100

    ok

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,14 +8,22 @@ import Foo from "@/app/components/Foo"
 import Cont from "@/app/components/Cont"
 import Mod from "@/app/components/Mod"
 
+import Na from "@/app/components/Na"
+
+
+
 
 export default function Home() {
   return (
     <div>
-      
+      <Na />
       <div className="flex flex-col h-screen">
+        <div className="">
+      
+      </div>
         <Nav />
         <Hero />
+     
 
         <div className="flex-grow flex items-center justify-center flex-wrap gap-8 mt-9 mb-9">
           <Card />
@@ -30,6 +38,7 @@ export default function Home() {
         </div>
         <Cont />
         <Mod />
+      
         <Foo />
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+const { nextui } = require('@nextui-org/react');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -14,5 +17,6 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui")],
-}
+  darkMode: 'class', // Añadido para que coincida con tu configuración actual
+  plugins: [require('daisyui'), nextui()],
+};
